ced057b0fbd165403f035723f220aadf7362b8c4
Brandon Desktop <brandonpowel35@gmail.com>
2021-06-05 13:43:54 -0400

    Initial commit

---
+++ src/main/java/ex04.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/*Exercise 4 - Mad Lib
+Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
+
+Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
+
+Example Output
+Enter a noun: dog
+Enter a verb: walk
+Enter an adjective: blue
+Enter an adverb: quickly
+Do you walk your blue dog quickly? That's hilarious!
+
+Constraints
+
+Use a single output statement for this program.
+If your language supports string interpolation or string substitution, use it to build up the output.
+
+Challenges
+
+Add more inputs to the program to expand the story.
+Implement a branching story, where the answers to questions determine how the story is constructed.
+ */
+public class ex04 {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[]args){
+
+        System.out.print("Enter a noun: ");
+        String noun = in.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = in.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = in.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adverb = in.nextLine();
+        System.out.println(String.format("Do you %s your %s %s %s? that's hilarious!", verb, adjective, noun, adverb));
+
+    }
+}
